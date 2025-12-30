8144ae6c364759d67cd2c2a2a0174bfe64ac37ed
Sirawit <sirawit.charo@ku.th>
2025-12-30 11:08:06 +0700

    add js

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+/*console.log("Hello, World!");
+console.log("This is the main.js file.");
+*/
+/*
+let fname = "John";
+let age = 30;
+let height = 5.9;
+
+console.log("Name: " + fname);
+console.log("age: ", age);
+console.log("Height: ", height);
+*/
+/*
+let number1 = 10;
+let number2 = 3;
+
+let result1 = number1 + number2;
+console.log("ผลบวก = ",result1);
+*/
+/*
+let number1 = 10;
+let number2 = 20;
+*/
+/*
+let condition = number1 < number2;
+console.log("condition: ", condition);
+*/
+/*
+let score = prompt("กรุณาใส่คะแนนของคุณ:");
+if (score >= 80) {
+    console.log("เกรด A");
+}else if (score >=70 ) {
+    console.log("เกรด B");
+} else if (score >=60 ) {
+    console.log("เกรด C");
+}else if (score >=50 ) {
+    console.log("เกรด D");
+}  else {
+    console.log("เกรด F");
+}
+*/
+/*
+if (number1 >= number2) {
+    console.log('this is if');
+}
+    else if (number1 == number2) {
+    console.log('this is else if');
+
+} else {
+    console.log('this is else');
+}
+*/
+/*
+let number1 = 5
+let number2 = 10
+
+let condition1 = (number1 >0) && (number2 > 0)
+console.log("condition: ", condition1);
+*/
+
+let conter = 0;
+while (conter <=4) {
+    conter = conter + 1;
+    console.log ("while: ", conter);
+}
