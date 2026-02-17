9a3c5e089961ef97c65fb4c69ca61d57d1572bbd
sirawit <sirawit.charo@ku.th>
2026-02-17 11:38:32 +0700

    Update api mock

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,76 @@
-//ทำการ import http module เพื่อสร้าง server
-const http = require('http')
-const host = 'localhost';
-const port = 8002;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000
+app.use(bodyParser.text());
+let users = []
+let counter = 1;
 
-//กำหนดค่าเริ่มต้นของ server เมื่อเปิดใช้งาน เว็บผ่านเบราซ์เซอร์
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    res.end('My First Server!');
-
-}
-//run server
-const server = http.createServer(requestListener);
-server.listen(port, host,() => {
-    console.log(`Server is running at http://${host}:${port}`);
+//path GET / users
+app.get('/users', (req, res) => {
+    res.json(users);
 });
+//path GET / test
+app.get('/test', (req, res) => {
+    let user = {
+        name: 'John Doe',
+        age: 30,
+        email: 'john.doe@example.com'
+    }
+    res.json(user)
+});
+
+//path POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter;
+    counter+= 1;
+    users.push(user);
+    res.json({
+        message: 'User added successfully',
+        user: user });
+})
+//path put /user/:id
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    //user จาก id ที่ส่งมา
+    let seletedIndex = users.findIndex(user => user.id == id);
+    
+    //อัพเดตข้อมูล user
+    if (updatedUser.name) {
+        users[seletedIndex].name = updatedUser.name;
+    }
+    if (updatedUser.email) {
+        users[seletedIndex].email = updatedUser.email;
+    }
+
+    //เอาข้อมูลที่ update ส่ง response กลับไป
+    res.json({
+        message: 'User updated successfully',
+        data: {
+            user: updatedUser,
+            indexUpdated: seletedIndex
+        }
+     });
+})
+
+//pate delete /user/:id
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+
+    // หา index ของ user ที่ต้องการลบจาก id ที่ส่งมา
+    let seletedIndex = users.findIndex(user => user.id == id);
+
+    //ลบ user ออกจาก array โดยใช้ delete
+    delete users[seletedIndex];
+    res.json({
+        message: 'User deleted successfully',
+            indexDeleted: seletedIndex
+     });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
+
+
